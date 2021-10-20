d2ecf637625a67fd7673e1cc66c1bafb91f95d4f
Ömer Faruk Köse <kazakh_sarbas@hotmail.com>
2021-10-20 22:52:09 +0300

    no message

--- TodoApp/ViewController.swift
+++ TodoApp/ViewController.swift
@@ -15,44 +15,85 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+
         tableView.delegate = self
         tableView.dataSource = self
-        // Do any additional setup after loading the view.
+
         getData()
     }
     
     func getData(){
         let tasks = DataBaseHelper.shareInstance.fetch()
-        
         taskStore = [tasks.filter{$0.isdone == false} , tasks.filter{$0.isdone == true}]
-        
         tableView.reloadData()
     }
 
     @IBAction func addButtonClicked(_ sender: Any) {
+        let alert = UIAlertController(title: "Add Task", message: nil, preferredStyle: .alert)
+        let add = UIAlertAction(title: "Add", style: .default) { _ in
+            guard let name = alert.textFields?.first?.text else {return}
+            if name != "" {
+                DataBaseHelper.shareInstance.save(name: name, isdone: false)
+            }
+            self.getData()
+        }
+        
+        let cancel = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        
+        alert.addTextField { textField in
+            textField.placeholder = "Enter task here..."
+        }
+        
+        alert.addAction(add)
+        alert.addAction(cancel)
+        present(alert, animated: true, completion: nil)
     }
     
 }
 
 extension ViewController: UITableViewDelegate ,UITableViewDataSource {
     
-    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return section == 0 ? "To-Do" : "Done"
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = taskStore[indexPath.section][indexPath.row].name
+        return cell
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
         return taskStore.count // 2 Dönüyor
     }
     
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return section == 0 ? "To Do" : "Done"
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return taskStore[section].count
     }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel?.text = taskStore[indexPath.section][indexPath.row].name
+    //right to left swipe
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let doneAction = UIContextualAction(style: .normal, title: nil) { (aciton, sourceView, completionHandler) in
+            let row = self.taskStore[0][indexPath.row]
+            DataBaseHelper.shareInstance.update(name: row.name!, isdone: true)
+            self.getData()
+        }
+        doneAction.backgroundColor = .green
         
-        return cell
+        return indexPath.section == 0 ? UISwipeActionsConfiguration(actions: [doneAction]) : nil
     }
- 
+    
+    
+    // left to right swipe
+    func tableView(_ tableView: UITableView, leadingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        
+        let deleteAction = UIContextualAction(style: .normal, title: nil) { (aciton, sourceView, completionHandler) in
+            let row = self.taskStore[indexPath.section][indexPath.row]
+            DataBaseHelper.shareInstance.delete(name: row.name!)
+            self.getData()
+        }
+        deleteAction.backgroundColor = .red
+        
+        return UISwipeActionsConfiguration(actions: [deleteAction])
+    }
 }
